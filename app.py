ddd725adf6e314be0130a2fc8bdc21d8e401e278
kelvin rushbrook <kelvinstudy@outlook.com>
2025-04-23 12:26:18 +0100

    Added bar charts and data tables , along with page change if loop

--- app.py
+++ app.py
@@ -3,8 +3,8 @@
 import streamlit as st
 import pandas as pd
 import pydeck as pdk
+import matplotlib.pyplot as plt
 
-st.title("Map of Meteor Landings")
 
 #region LOAD DATASET
 df = pd.read_csv(r"D:\GitMeteor\Meteorite_Landings.csv")
@@ -19,6 +19,13 @@ df = df.dropna(subset=['mass_g'])
 #endregion
 
 #region SETUP SIDEBAR
+
+#Setup navigation bar to change pages - added pages as overview page was getting cramped
+page = st.sidebar.selectbox(
+    "Navigate to:",
+    ["Overview", "Charts", "Data Tables"]
+)
+
 #add sidebar title
 st.sidebar.header("Filter Options")
 
@@ -80,59 +87,100 @@ if selected_class:
     df = df[df['recclass'] == selected_class]
 #endregion
 
-#region SETUP MAP
-# Drop rows with missing coordinates 
-df = df.dropna(subset=['reclong', 'reclat','GeoLocation'])
-# Create Pydeck scatterplot for map
-layer = pdk.Layer(
-    "ScatterplotLayer",
-    data=df,
-    get_position='[reclong, reclat]',  
-    get_color='[200, 0, 0, 255]',   # Red color,gree,blue ,tranparancy
-    get_radius=3000,                        
-    pickable=True,
-)
+if page == "Overview":
 
-# Add view state to center the map  
-view_state = pdk.ViewState(
-    latitude=df['reclat'].mean(),
-    longitude=df['reclong'].mean(),
-    zoom=1,
-    pitch=0,
-)
+    #region SETUP MAP
+    st.title("Map of Meteor Landings")
+    # Drop rows with missing coordinates 
+    df = df.dropna(subset=['reclong', 'reclat','GeoLocation'])
+    # Create Pydeck scatterplot for map
+    layer = pdk.Layer(
+        "ScatterplotLayer",
+        data=df,
+        get_position='[reclong, reclat]',  
+        get_color='[200, 0, 0, 255]',   # Red color,gree,blue ,tranparancy
+        get_radius=3000,                        
+        pickable=True,
+    )
 
-# Tooltips on map when hovering mouse over
-tooltip = {
-    "html": "<b>Latitude:</b> {reclat} <br/><b>Longitude:</b> {reclong} <br/><b>Name:</b> {name} <br/><b>Year of impact:</b> {year} <br/><b>mass (g):</b> {mass_g} <br/><b>Class (g):</b> {recclass}"
-}
+    # Add view state to center the map  
+    view_state = pdk.ViewState(
+        latitude=df['reclat'].mean(),
+        longitude=df['reclong'].mean(),
+        zoom=1,
+        pitch=0,
+    )
 
-# Show map
-st.pydeck_chart(pdk.Deck(layers=[layer], initial_view_state=view_state, tooltip=tooltip))
+    # Tooltips on map when hovering mouse over
+    tooltip = {
+        "html": "<b>Latitude:</b> {reclat} <br/><b>Longitude:</b> {reclong} <br/><b>Name:</b> {name} <br/><b>Year of impact:</b> {year} <br/><b>mass (g):</b> {mass_g} <br/><b>Class (g):</b> {recclass}"
+    }
 
-#endregion
+    # Show map
+    st.pydeck_chart(pdk.Deck(layers=[layer], initial_view_state=view_state, tooltip=tooltip))
 
-#region SETUP KPIS 
+    #endregion
 
-#add dashboard header
-st.subheader("Key Statistics")
+    #region SETUP KPIS 
 
-#create 4 columns , 1 for each kpi
-kpi1,kpi2,kpi3,kpi4 = st.columns(4)
+    #add dashboard header
+    st.subheader("Key Statistics")
 
-#create metric for total meteors and display on column kpi1
-kpi1.metric("Total Meteors",len(df))
+    #create 4 columns , 1 for each kpi
+    kpi1,kpi2,kpi3,kpi4 = st.columns(4)
 
-#calculate average mass of meteors
-avgMass = df['mass_g'].mean()
-#create metric for average mass and rounded to whole number and adding a comma , as the mass in grams is way too large to display neatly
-kpi2.metric("Average Mass in grams",f"{avgMass:,.0f}")
+    #create metric for total meteors and display on column kpi1
+    kpi1.metric("Total Meteors",len(df))
 
-#create metric for Maximum mass
-maxMass = df['mass_g'].max()
-kpi3.metric("Maximum Mass in grams",f"{maxMass:,.0f}")
+    #calculate average mass of meteors
+    avgMass = df['mass_g'].mean()
+    #create metric for average mass and rounded to whole number and adding a comma , as the mass in grams is way too large to display neatly
+    kpi2.metric("Average Mass in grams",f"{avgMass:,.0f}")
 
-#create metric for minumum mass
-minMass = df['mass_g'].min()
-kpi4.metric("Minimum Mass in grams",f"{minMass:,.5f}")
+    #create metric for Maximum mass
+    maxMass = df['mass_g'].max()
+    kpi3.metric("Maximum Mass in grams",f"{maxMass:,.0f}")
 
-#endregion
+    #create metric for minumum mass
+    minMass = df['mass_g'].min()
+    kpi4.metric("Minimum Mass in grams",f"{minMass:,.5f}")
+
+    #endregion
+
+elif page == "Charts":
+
+    #region SETUP CHARTS
+    st.title("Data Visualisations")
+
+    #Create  top 10 classes chart
+    st.subheader("Top 10 Meteor classes")
+    topClasses = df["recclass"].value_counts().sort_values(ascending=False).head(10)
+    st.bar_chart(topClasses)
+
+    #Create average mass by year line chart
+    st.subheader("Average Mass by Year")
+    st.write("You can zoom in or out for a better view")
+    avg_mass_by_year = df.groupby(df['year'].astype(int))['mass_g'].mean()
+    avg_mass_by_year.index = avg_mass_by_year.index.astype(str)
+    st.line_chart(avg_mass_by_year)
+    #Create mass by class bar chart 
+    st.subheader("Top 5 Heaviest Meteorites by class")
+    mass_by_class = df.groupby('recclass')['mass_g'].mean().sort_values(ascending=False).head(10)
+    mass_by_class.index = mass_by_class.index.astype(str)
+    st.bar_chart(mass_by_class)
+    
+    #endregion
+
+elif page == "Data Tables":
+    
+    #add heaviest meteor table
+    st.subheader("Top 5 Heaviest Meteorites")
+    top_5H = df.sort_values(by='mass_g', ascending=False).head(5)
+    top_5H['year'] = top_5H['year'].astype(int).astype(str) #convert year string to remove commas but conver to int first otherwise get trailing 0s like 1920.0
+    st.table(top_5H[['name', 'mass_g', ('year'), 'recclass',]])
+
+    #add most common meteor class table
+    st.subheader("Top 5 Most Common Meteorite Classes")
+    top_classes = df['recclass'].value_counts().head(5)
+    top_classes.columns = ['Class', 'Count']
+    st.table(top_classes)
